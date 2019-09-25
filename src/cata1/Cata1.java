db7e17326b33489236d796f8253aadca1669bc39
Usuario <Usuario@10.22.148.97>
2019-09-25 13:52:05 +0100

    Package finished

--- src/cata1/Cata1.java
+++ src/cata1/Cata1.java
@@ -1,10 +1,13 @@
 
 package cata1;
 
+import java.util.Date;
+
 public class Cata1 {
     
     public static void main(String[] args) {
-        
+        Person pau = new Person("Pau", new Date(99,5,7));
+        System.out.println(pau.getAge());
     }
     
 }
